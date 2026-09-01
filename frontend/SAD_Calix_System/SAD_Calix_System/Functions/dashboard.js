44489b7ac73afd58c2c731ac5c90e16469be6879
Rojiry <102029658+Rojiry@users.noreply.github.com>
2026-09-01 23:30:30 +0800

    Delete frontend/SAD_Calix_System/SAD_Calix_System directory

--- frontend/SAD_Calix_System/SAD_Calix_System/Functions/dashboard.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.getElementById("menuButton");
-const sidebar = document.querySelector(".sidebar");
-
-menuButton.addEventListener("click", function () {
-    sidebar.classList.toggle("open");
-});
